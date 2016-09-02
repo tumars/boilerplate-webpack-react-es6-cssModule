ae5a866a0211b24582bebbc7cf8b3e3d77659878
menghui9898 <menghui9898@gmail.com>
2016-09-02 13:43:36 +0800

    remove fetch-mock comments

--- src/container/FundListpage/FundListContainer.js
+++ src/container/FundListpage/FundListContainer.js
@@ -5,27 +5,12 @@ import { browserHistory } from 'react-router'
 import 'whatwg-fetch'
 // import fetchJsonp from 'fetch-jsonp'  如果使用 jsonp，使用此依赖，方法查看 https://github.com/camsong/fetch-jsonp
 
-// import fetchMock from 'fetch-mock'
-// import makeRequest from './make-request'
-
-
 import FundList from './FundListComponent.js'
-
-
-
-// fetchMock.get('*', {"inall": 2,"index": 1,"data": [{"name": "肖申克的救赎"},{"name": "这个杀手不太冷"},{"name": "霸王别姬"},{"name": "阿甘正传"},{"name": "美丽人生"}]});
-// makeRequest().then(function(data) {
-//   console.log(data);
-// });
-
-
-
 
 
 const mapDispatchToProps = (dispatch) => {
 	return{
 		dataInit: () => {
-			// const result = fetch('../pagedata/page'+1+'.json')
 			const result = fetch('http://localhost:3003/page'+1)
 			result.then(res => res.json())
 			.then(MovieData => {
